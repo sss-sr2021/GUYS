a006ca493c4d36206002920482c659d5fcfca2d4
花岡 宗史 <hanaoka@sr-co.co.jp>
2021-05-20 15:13:48 +0900

    custom shop 軽い修正

--- www/Pages/custom.php
+++ www/Pages/custom.php
@@ -5,7 +5,7 @@
  * Author:伊藤明洋
  * Version :0.0.1
  * create :2021.05.20
- * Update :2021.05.20 伊藤
+ * Update :2021.05.20 花岡
  * 
  * 
 */
@@ -17,20 +17,19 @@ $pageTitle = "カスタム";
 $h1 ="カスタム";
 include('./commonParts/header.php');
 ?>
-     <div class='container'>
-         <main>
+    <div class='container'>
+        <main>
             <h2>カスタムテーマ</h2>
-                <p>現在のテーマ：theme1</p> <!-- 設定されている現在のテーマ名を表示する。参照先：userテーブル(currentTheme) -->
+                <p>現在のテーマ：<span >/p> <!-- 設定されている現在のテーマ名を表示する。参照先：userテーブル(currentTheme) -->
                 <a href="shop.php">ショップ</a>
-                <hr>
                 <img src="" alt="theme1" /> <!-- ユーザーのが所持済のカスタムテーマを表示する。参照先：shopInfoテーブル -->
                 <p>theme1</p>
                 <img src="" alt="theme2" />
                 <p>theme2</p>
                 <img src="" alt="theme3" />
                 <p>theme3</p>
-         </main>
-     </div>
+        </main>
+    </div>
 <?php
 include('./commonParts/footer.php');
 ?>
--- www/Pages/shop.php
+++ www/Pages/shop.php
@@ -5,7 +5,7 @@
  * Author:伊藤明洋
  * Version :0.0.1
  * create :2021.05.20
- * Update :2021.05.20 伊藤
+ * Update :2021.05.20 花岡
  * 
  * 
 */
@@ -17,18 +17,17 @@ $pageTitle = "ショップ";
 $h1 ="ショップ";
 include('./commonParts/header.php');
 ?>
-     <div class='container'>
-         <main>
+    <div class='container'>
+        <main>
             <h2>カスタムテーマショップ</h2>
                 <p>所持ポイント：〇〇pt</p>
                 <a href="custom.php">カスタム</a>
-                <hr>
                 <img src="" alt="theme2" /> <!-- 未所持のカスタムテーマの表示。ユーザーのが未所持のカスタムテーマを表示する。参照先：shopInfoテーブル -->
                 <p>theme2：100pt</p>
                 <img src="" alt="theme3" />
                 <p>theme3：100pt</p>
-         </main>
-     </div>
+        </main>
+    </div>
 <?php
 include('./commonParts/footer.php');
 ?>
