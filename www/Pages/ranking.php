1c64d6de7f13f931202203d4dd01fc3bb64cc15f
花岡 宗史 <hanaoka@sr-co.co.jp>
2021-05-20 17:24:37 +0900

    ランキングページ累計奮闘中

--- www/Pages/ranking.php
+++ www/Pages/ranking.php
@@ -7,10 +7,39 @@
  * create :2021.05.20
  * Update :2021.05.20 花岡
  * 
+ * $_SESSION['span']//指定した期間
  * 
 */
 
     require_once './commonParts/functions.php';
+    @session_start();
+    
+    $dbh=dbInit();
+    if(empty($_SESSION['span'])){
+        $_SESSION['span']="週間";
+    }
+
+    if(isset($_POST['weeklyButton'])) {
+        $_SESSION['span']="週間";
+    }
+    elseif(isset($_POST['monthlyButton'])) {
+        $_SESSION['span']="月間";
+    }
+    elseif(isset($_POST['yearlyButton'])) {
+        $_SESSION['span']="年間";
+    }
+    elseif(isset($_POST['totalButton'])) {
+        $_SESSION['span']="累計";
+        $sql2= "SELECT SUM(mileage) from milage where user_id=2";
+        $sql = 'SELECT user_id FROM mileage ORDER BY mileage DESC';
+        var_dump($sql2);
+        //データベースを検索
+        foreach($dbh->query($sql) as $row){
+            var_dump($row);
+        }
+        //select category,SUM(price) as average from products group by category
+        //https://www.ipentec.com/document/sql-get-sum
+    }
 ?>
 <?php
 $pageTitle = "ランキング";
@@ -20,34 +49,36 @@ include('./commonParts/header.php');
     <div class='container'>
     <script src="/jquery-3.6.0.js"></script>
         <script>
-            $(function(){
-                $("#weekly").click(function(){
-                    $("h2").html("週間ランキング");
-                });
-            });
-            $(function(){
-                $("#monthly").click(function(){
-                    $("h2").html("月間ランキング");
-                });
-            });
-            $(function(){
-                $("#yearly").click(function(){
-                    $("h2").html("年間ランキング");
-                });
-            });
-            $(function(){
-                $("#total").click(function(){
-                    $("h2").html("累計ランキング");
-                });
-            });
+            // $(function(){
+            //     $("#weekly").click(function(){
+            //         $("h2").html("週間ランキング");
+            //     });
+            // });
+            // $(function(){
+            //     $("#monthly").click(function(){
+            //         $("h2").html("月間ランキング");
+            //     });
+            // });
+            // $(function(){
+            //     $("#yearly").click(function(){
+            //         $("h2").html("年間ランキング");
+            //     });
+            // });
+            // $(function(){
+            //     $("#total").click(function(){
+            //         $("h2").html("累計ランキング");
+            //     });
+            // });
         </script>
         <main>
-            <h2>週間ランキング</h2>
+            <h2><?= $_SESSION['span'] ?>ランキング</h2>
                 <label for="spanButton">
-                    <button name="spanButton" id="weekly">週間</button>
-                    <button name="spanButton" id="monthly">月間</button>
-                    <button name="spanButton" id="yearly">年間</button>
-                    <button name="spanButton" id="total">累計</button>
+                    <form action="ranking.php" method="post">
+                        <button type="submit" name="weeklyButton" id="weekly">週間</button>
+                        <button type="submit" name="monthlyButton" id="monthly">月間</button>
+                        <button type="submit" name="yearlyButton" id="yearly">年間</button>
+                        <button type="submit" name="totalButton" id="total">累計</button>
+                    </form>
                 </label>
                 <label for="yourRank">
                     <p>あなたは<span class="rankingRank">位</span><span class="rankingMileage">km</span></p>
