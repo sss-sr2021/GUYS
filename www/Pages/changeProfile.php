9a33c51d8af2a53c1ee9679680192426d654715c
伊藤 <itou@sr-co.co.jp>
2021-05-20 17:22:11 +0900

    changeProfile変更できるようにした、パスワードの承認を削除

--- www/Pages/changeProfile.php
+++ www/Pages/changeProfile.php
@@ -3,14 +3,36 @@
  * changeProfile.php :プロフィール情報変更画面
  * 
  * Author:伊藤明洋
- * Version :0.0.1
+ * Version :0.1.1
  * create :2021.05.20
  * Update :2021.05.20 伊藤
- * 
+ * $user :セッションにあるユーザー情報
  * 
 */
 
     require_once './commonParts/functions.php';
+    session_start();
+    $user = $_SESSION['logined'];  //セッションにあるユーザー情報
+
+    //パスワードを基準にして名前と性別を変更
+    if(!empty($_POST['change'])){  //もしsubmitにの値が空じゃなければ以下を実行する
+        $dbh = dbInit();  //functionsからデータベースとの接続関数を持ってくる
+        $sth = $dbh-> prepare(  //passが同じならパスワードを変更する
+            'UPDATE users SET name = :name , gender = :gender WHERE /*pass = :pass and*/ id = :id'
+        );
+        $ret = $sth->execute([
+             'name' => (string)filter_input(INPUT_POST,'name'),  //:nameは入力された値を入れる
+             'gender' => filter_input(INPUT_POST,'gender'), //:genderは選択された値を入れる
+             //パスワードの確認欄は削除
+             //'pass' => password_hash(filter_input(INPUT_POST,'password'),PASSWORD_DEFAULT), //:pass入力された値を入れる、暗号化も
+             'id' => filter_input(INPUT_POST,'id')  //:idはhiddenで取ってきた値を入れる
+         ]);
+         echo("変更しました。"); //後々ここにアラートを追加予定
+    }
+    else{
+        "";
+    }
+
 ?>
 <?php
 $pageTitle = "プロフィール情報変更";
@@ -21,14 +43,17 @@ include('./commonParts/header.php');
          <main>
             <h2>プロフィール情報変更</h2>
                 <form action="" method="post">
-                    <p>お名前：<input type="text" name="email" required></p>
+                    <p>お名前：<input type="text" name="name" required></p>
                     <p>性別：
-                    <label><input type="radio" name="gender" value="1" required/>男</label>
-                    <label><input type="radio" name="gender" value="2" />女</label>
-                    <label><input type="radio" name="gender" value="3" />その他</label>
+                    <label><input type="radio" name="gender" value="M" required/>男</label>
+                    <label><input type="radio" name="gender" value="F" />女</label>
+                    <label><input type="radio" name="gender" value="O" />その他</label>
                     </p>
-                    <p>パスワード：<input type="password" name="password" required></p>
-                    <p><input type="submit" name="submit" value="変更"></p>
+                    <!-- <p>パスワード：<input type="password" name="password" required></p> -->
+                    <p><input type="submit" name="change" value="変更"></p>
+                    <!-- セッションのユーザーIDをhiddenで取ってくる -->
+                    <p><br />
+                    <input type="hidden" name="id" value="<?= $user["id"] ?>"/></p>
                 </form>
          </main>
      </div>
