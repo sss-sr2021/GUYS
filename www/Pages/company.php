4f1403fcc8128ed0fdefb9578e6e6c23dd679b7a
秦 <hata@sr-co.co.jp>
2021-05-20 13:20:16 +0900

    運営会社ページ追加 プライバシーポリシー追加

---
+++ www/Pages/company.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * createAccount.php :運営会社画面
+ * 
+ * Author:秦伊吹
+ * Version :0.0.1
+ * create :2021.05.20
+ * Update :
+ * 
+ * 
+*/
+
+    require_once './commonParts/functions.php';
+?>
+<?php
+$pageTitle = "漢委員会";
+$h1 ="漢委員会";
+include('./commonParts/header.php');
+?>
+    <div class='container'>
+        <main>
+            <h2>漢委員会概要</h2>
+                <div>
+                </div>
+        </main>
+    </div>
+<?php
+include('./commonParts/footer.php');
+?>
