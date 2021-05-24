a67d2cc9f238ed29f11c5854746833cba65a8b2f
伊藤 <itou@sr-co.co.jp>
2021-05-24 16:59:09 +0900

    ショップ完成(?),カスタム60%完成

--- www/Pages/custom.php
+++ www/Pages/custom.php
@@ -7,11 +7,36 @@
  * create :2021.05.20
  * Update :2021.05.20 花岡
  * 
+ * $theme :テーマを1(所持)か0(未所持)かで入れる
  * 
 */
 
     require_once './commonParts/functions.php';
     loginBi();//ログインしていなければログインページへ遷移
+    $user = $_SESSION['logined'];
+    //shop_infoからデータを取得
+    $dbh = dbInit();
+    //セッションのIDとデータベースのuser_idを照合
+    $sql = "SELECT * FROM shop_info WHERE user_id = :user_id";
+    $sth = $dbh->prepare($sql);     //shop_infoテーブルの中のすべての列を検索
+    $exc = $sth->execute([
+        'user_id' => $user["id"] //セッションのID
+    ]);
+    $rows = $sth->fetchAll();
+
+    //usersからcurrent_themeを取得
+    $dbh = dbInit();
+    //セッションのIDとデータベースのidを照合
+    $sql2 = "SELECT current_theme FROM users WHERE id = :id";
+    $sth = $dbh->prepare($sql2);
+    $exc = $sth->execute([
+        'id' => $user["id"] //セッションのID
+    ]);
+    $rows2 = $sth->fetchAll();
+
+    $theme[1] = $rows[0]["theme1"];
+    $theme[2] = $rows[0]["theme2"];
+    $theme[3] = $rows[0]["theme3"];
 ?>
 <?php
 $pageTitle = "カスタム";
@@ -21,14 +46,18 @@ include('./commonParts/header.php');
     <div class='container'>
         <main>
             <h2>カスタムテーマ</h2>
-                <p>現在のテーマ：<span >/p> <!-- 設定されている現在のテーマ名を表示する。参照先：userテーブル(currentTheme) -->
-                <a href="shop.php">ショップ</a>
-                <img src="" alt="theme1" /> <!-- ユーザーのが所持済のカスタムテーマを表示する。参照先：shopInfoテーブル -->
-                <p>theme1</p>
-                <img src="" alt="theme2" />
-                <p>theme2</p>
-                <img src="" alt="theme3" />
-                <p>theme3</p>
+                <p>現在のテーマ：theme<?= $rows2[0]["current_theme"] ?> </p> <!-- 設定されている現在のテーマ名を表示する。参照先：userテーブル(currentTheme) -->
+                <a href="shop.php">ショップ</a><br />
+                <?php
+                //ユーザーのが所持済のカスタムテーマを表示する。参照先：shop_infoテーブル
+                    foreach($theme as  $key => $value){
+                        if(empty($value)){  //空(0(未所持))だったらスキップして次のthemeの処理へ
+                            continue;
+                        }
+                        echo "<img src='../images/theme{$key}.png'  alt='theme{$key}'width='500'height='280' />" . 
+                        "<p>theme{$key}</p>" ;
+                    }
+                ?>
         </main>
     </div>
 <?php
--- www/Pages/shop.php
+++ www/Pages/shop.php
@@ -7,12 +7,63 @@
  * create :2021.05.20
  * Update :2021.05.20 花岡
  * 
+ * $getTheme :取得してきたテーマ(1,2,3が入ってる)
  * 
 */
 
     require_once './commonParts/functions.php';
     loginBi();//ログインしていなければログインページへ遷移
+    $user = $_SESSION['logined'];
+
+
+    if(@$_POST['point']){
+        //usersからpointを100減らす
+        $dbh = dbInit();
+        $sql2 = "UPDATE users SET point=point-100 WHERE id = :id"; //セッションのIDとデータベースのidを照合
+        $sth = $dbh->prepare($sql2);
+        $exc = $sth->execute([
+            'id' => $user["id"], //セッションのID
+        ]);
+        $rows2 = $sth->fetchAll();
+
+        //shop_infoからデータを取得
+        $getTheme = (integer)filter_input(INPUT_POST, 'theme');
+        $dbh = dbInit();
+        //セッションのIDとデータベースのuser_idを照合
+        if($getTheme > 0){ //未所持の0
+        $sql = "UPDATE shop_info SET theme{$getTheme}=1 WHERE user_id = :user_id"; //所持済みの１
+        $sth = $dbh->prepare($sql);     //shop_infoテーブルの中のすべての列を検索
+        $exc = $sth->execute([
+            'user_id' => $user["id"], //セッションのID
+        ]);
+        $rows = $sth->fetchAll();
+    }}
+
+    //shop_infoからデータを取得
+    $dbh = dbInit();
+    //セッションのIDとデータベースのuser_idを照合
+    $sql = "SELECT * FROM shop_info WHERE user_id = :user_id";
+    $sth = $dbh->prepare($sql);     //shop_infoテーブルの中のすべての列を検索
+    $exc = $sth->execute([
+        'user_id' => $user["id"] //セッションのID
+    ]);
+    $rows = $sth->fetchAll();
     
+
+
+    //usersからpointを取得
+    $dbh = dbInit();
+    //セッションのIDとデータベースのidを照合
+    $sql2 = "SELECT point FROM users WHERE id = :id";
+    $sth = $dbh->prepare($sql2);
+    $exc = $sth->execute([
+        'id' => $user["id"] //セッションのID
+    ]);
+    $rows2 = $sth->fetchAll();
+
+    $theme[1] = $rows[0]["theme1"];
+    $theme[2] = $rows[0]["theme2"];
+    $theme[3] = $rows[0]["theme3"];
 ?>
 <?php
 $pageTitle = "ショップ";
@@ -21,13 +72,38 @@ include('./commonParts/header.php');
 ?>
     <div class='container'>
         <main>
+            <script>
+                
+                function imgClick(theme){ //画像がクリックされた時
+                    if(<?= $rows2[0]["point"] ?> < 100){ //ポイント数が100より小さい時にポイントが足らないアラート
+                        alert('ポイントが足りません（所持ポイント数：<?= $rows2[0]["point"] ?>pt）');
+                    }
+                    else{
+                        var result = confirm('購入しますか？（購入後のポイント数：<?= $rows2[0]["point"] -100 ?>pt）');
+                        if(result){
+                            $('#theme').val(theme);
+                            $('#themeForm').submit();
+                        }
+                    }
+                }
+            </script>
             <h2>カスタムテーマショップ</h2>
-                <p>所持ポイント：〇〇pt</p>
-                <a href="custom.php">カスタム</a>
-                <img src="images/theme2.png" alt="theme2" /> <!-- 未所持のカスタムテーマの表示。ユーザーのが未所持のカスタムテーマを表示する。参照先：shopInfoテーブル -->
-                <p>theme2：100pt</p>
-                <img src="images/theme3.png" alt="theme3" />
-                <p>theme3：100pt</p>
+                <p>所持ポイント：<?= $rows2[0]["point"] ?>pt</p>
+                <a href="custom.php">カスタム</a><br />
+                <?php
+                //未所持のカスタムテーマの表示。ユーザーのが未所持のカスタムテーマを表示する。参照先：shop_infoテーブル
+                    foreach($theme as  $key => $value){
+                        if(!empty($value)){  //0(未所持)の場合のみ表示
+                            continue;
+                        }
+                        echo "<img src='../images/theme{$key}.png ' alt='theme{$key}' name='{$key}' width='500' height='280' onclick='imgClick({$key})' />" . 
+                        "<p>theme{$key}:100pt</p>" ;   
+                    }
+                ?>
+                <form id="themeForm" method="post"> <!-- 自分に送ってる -->
+                    <input type="hidden" id="theme" name="theme" value="">
+                    <input type="hidden" id="point" name="point" value="-100">
+                </form>
         </main>
     </div>
 <?php
