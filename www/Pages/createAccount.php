6320fe888a98d77d10bf10df8f3cea5f8a0c2c0d
秦 <hata@sr-co.co.jp>
2021-05-27 08:55:18 +0900

    createAccount.php変更

--- www/Pages/createAccount.php
+++ www/Pages/createAccount.php
@@ -7,7 +7,7 @@
  * create :2021.05.19
  * Update :2021.05.22 花岡//同一メールアドレスで複数のアカウント作成はできないように修正
  *         2021.05.23 花岡//アカウント作成時、shop_infoテーブルにもデータを追加
- *         2021.05.25 秦//アカウント作成の際の、パスワード入力の制限桁数、制限文字の追加
+ *         2021.05.25 秦//アカウント作成の際の、パスワード入力の制限桁数、制限文字の追加、名前入力の桁数制限追加。
  * 
  * 
 */
@@ -55,7 +55,7 @@ include('./commonParts/header.php');
             <p><?= $message ?></p>
                 <form action="createAccount.php" method="post">
                     <p>メールアドレス：<input type="email" name="email" required></p>
-                    <p>パスワード：<input type="password" name="password" pattern="[a-zA-Z0-9]{8,16}" required></p>
+                    <p>パスワード：<input type="password" name="password" pattern="[a-zA-Z0-9]{8,16}" placeholder="半角英数字8～16文字" required></p>
                     <p>お名前：<input type="text" name="name" pattern=".{1,20}" required></p>
                     <p>性別：
                     <label><input type="radio" name="gender" value="M" required/>男</label>
